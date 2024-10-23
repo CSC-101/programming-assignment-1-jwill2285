d1b6b49a8c26f1b7068940d905adc1c754ebe0ba
joey <jwill226@calpoly.edu>
2024-10-23 02:21:07 -0700

    1-4 done

--- hw1.py
+++ hw1.py
@@ -3,15 +3,60 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-def vowel_count
+def vowel_count(string:str)->int:
+    vowelCounter = 0
+    '''for loop reads each character in string and runs through the code for that many strings
+     if statement reads the character and will only continue if that character is part of the 
+     "aeiouAEIOU" listo of characters and adds 1 to the counter'''
+    for var in string:
+        if var in "aeiouAEIOU":
+            vowelCounter +=1
+    #returns the final count of vowels
+    return vowelCounter
 
 # Part 2
-
+def short_lists(lists : list[list[int]])->list[int]:
+    shorter_list = []
+    '''for statment loops through the list of lists, going through each list and checking with the if statement
+    if the length of the list is 2. If the length is 2, then it adds both values in the list to the new list that is made
+    finally returns what the new list is'''
+    for i in range(len(lists)):
+        if len(lists[i]) == 2:
+            shorter_list.append(lists[i][0])
+            shorter_list.append(lists[i][1])
+    return shorter_list
 
 # Part 3
-
+def ascending_pairs(lists : list[list[int]]) ->list[int]:
+    ascending_list = []
+    '''for loop goes through each of the lists and then checks with the if statement if their length is 2.
+    if this is true then it checks if the first value is greater than the second value and if this is true
+    then it switches those values on line 40 then adds the change to the list. If the if statement is false 
+    then it just adds the list that has two values as is. For other lists that have more or less than two they
+    are just added to the list as is in line 45'''
+    for i in range(len(lists)):
+        if len(lists[i]) == 2:
+            if lists[i][0] > lists[i][1]:
+                lists[i][0], lists[i][1] = lists[i][1], lists[i][0]
+                ascending_list.append(lists[i])
+            else:
+                ascending_list.append(lists[i])
+        else:
+            ascending_list.append(lists[i])
+    return ascending_list
 
 # Part 4
+def add_prices(price1 : data.Price, price2: data.Price)->data.Price:
+    '''Line: 55 takes the total dollar amount from both of the inputs and adds them together
+    Line: 56 takes the total cents amount from both of the inputs and adds them together
+    Line: 57 takes the total dollar amount and adds the number of times that the cents is divisable by 100
+    Line: 58 takes the remainder of the cents that are divided by 100
+    return value returns the new price with total of dollars and cents'''
+    total_dollars = price1.dollars + price2.dollars
+    total_cents = price1.cents + price2.cents
+    total_dollars += total_cents //100
+    total_cents = total_cents % 100
+    return total_dollars,total_cents
 
 
 # Part 5
--- hw1_test.py
+++ hw1_test.py
@@ -7,15 +7,53 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
-
+    def test_vowel_count(self):
+        input = "Billy"
+        result = hw1.vowel_count(input)
+        expected = 1
+        self.assertEqual(expected,result)
+    def test_vowel_count_2(self):
+        input = "MANY vowels IN this SEntenCE"
+        result = hw1.vowel_count(input)
+        expected = 8
+        self.assertEqual(expected,result)
 
     # Part 2
-
-
+    def test_short_lists(self):
+        input = [[4,3],[4,2],[9,2,3,1],[10,2], [6,4,3,5]]
+        result = hw1.short_lists(input)
+        expected = [4,3,4,2,10,2]
+        self.assertEqual(expected,result)
+    def test_short_lists_2(self):
+        input = [[],[1,2],[3,4,5],[3,4],[99,100]]
+        result = hw1.short_lists(input)
+        expected = [1,2,3,4,99,100]
+        self.assertEqual(expected,result)
     # Part 3
-
+    def test_ascending_pairs(self):
+        input = [[4,3],[3,4,5],[1,2],[0,10,5],[10,0]]
+        result = hw1.ascending_pairs(input)
+        expected = [[3,4],[3,4,5],[1,2],[0,10,5],[0,10]]
+        self.assertEqual(expected,result)
+    def test_ascending_pairs_2(self):
+        input = [[1,2],[4,3],[100,200,30],[6,5]]
+        result = hw1.ascending_pairs(input)
+        expected = [[1,2],[3,4],[100,200,30],[5,6]]
+        self.assertEqual(expected,result)
 
     # Part 4
+    def test_add_prices(self):
+        input1 = data.Price(2,60)
+        input2 = data.Price(1,80)
+        result = hw1.add_prices(input1,input2)
+        expected = 4,40
+        self.assertEqual(expected,result)
+    def test_add_prices_2(self):
+        input1 = data.Price(100,45)
+        input2 = data.Price(50,55)
+        result = hw1.add_prices(input1,input2)
+        expected = 151,0
+        self.assertEqual(expected,result)
 
 
     # Part 5
