2c7b101ee195681aeb880607736844d3864f7e6e
joey <jwill226@calpoly.edu>
2024-10-23 19:50:17 -0700

    1-4 done

--- hw1_test.py
+++ hw1_test.py
@@ -55,20 +55,55 @@ class TestCases(unittest.TestCase):
         expected = 151,0
         self.assertEqual(expected,result)
 
-
     # Part 5
-
+    def test_rectangle_area(self):
+        input = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
+        result = hw1.rectangle_area(input)
+        expected = 50
+        self.assertEqual(expected,result)
+    def test_rectangle_area_2(self):
+        input = data.Rectangle(data.Point(5,5), data.Point(-5,-5))
+        result = hw1.rectangle_area(input)
+        expected = 100
+        self.assertEqual(expected, result)
 
     # Part 6
-
+    def test_books_by_author(self):
+        input1 = 'David Hasslehoff'
+        input2 = [data.Book(['David Hasslehoff'], 'Good Omens'), data.Book(['Bob Frank'], 'The Teaching')]
+        result = hw1.books_by_author(input1,input2)
+        expected = ['Good Omens']
+        self.assertEqual(expected,result)
+    def test_books_by_author_2(self):
+        input1 = 'Spongebob Squarepants'
+        input2 = [data.Book(['David Hasslehoff'],'Good Omens'),data.Book(['Spongebob Squarepants'], 'Bikini Bottom'), data.Book(['Spongebob Squarepants'], 'How to make Krabby Patties')]
+        result = hw1.books_by_author(input1,input2)
+        expected = ['Bikini Bottom', 'How to make Krabby Patties']
+        self.assertEqual(expected,result)
 
     # Part 7
-
+    def test_circle_bound(self):
+        input = data.Rectangle(data.Point(1,2), data.Point(3,4))
+        result = hw1.circle_bound(input)
+        expected = 1.4142135623730951
+        self.assertEqual(expected,result)
+    def test_circle_bound_2(self):
+        input = data.Rectangle(data.Point(-5,10), data.Point(3,-4))
+        result = hw1.circle_bound(input)
+        expected = 9.219544457292887
+        self.assertEqual(expected,result)
 
     # Part 8
-
-
-
+    def test_below_pay_average(self):
+        input = [data.Employee('Bob',100),data.Employee('Billy',200),data.Employee('Frank',300)]
+        result = hw1.below_average_pay(input)
+        expected = ['Bob']
+        self.assertEqual(expected,result)
+    def test_below_pay_average_2(self):
+        input = [data.Employee('Becky', 100000), data.Employee('Charles',200000), data.Employee('Derek',20), data.Employee('Stella',500)]
+        result = hw1.below_average_pay(input)
+        expected = ['Derek', 'Stella']
+        self.assertEqual(expected,result)
 
 
 if __name__ == '__main__':
